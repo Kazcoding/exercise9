f8372b590c25f3133909eaeb9a596bc0532f25f3
Kazcoding <77779320+Kazcoding@users.noreply.github.com>
2021-01-25 20:09:48 +0000

    Create types.py

---
+++ types.py
@@ -0,0 +1,7 @@
+Firstname = "Kathryn"
+Lastname = "Goulding"
+print(Firstname, Lastname)
+my_list = [Firstname, Lastname]
+print(my_list)
+my_dictionary = {"first": "Kathryn", "last": "Goulding"}
+print(my_dictionary["last"])
